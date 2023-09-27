a6c0d26e9258f6599fef705175be40d2a7a7c070
nitingss11 <nitingss11@gmail.com>
2023-09-26 21:06:21 -0400

    Modified the bug fix

--- MajorityMultipass.java
+++ MajorityMultipass.java
@@ -1,7 +1,6 @@
-import java.io.*;
 import java.util.* ;
 
-public class Solution {
+public class MajorityMultipass {
     public static int findMajority(int[] nums, int n) {
 
         List<Integer> A = new ArrayList<>();
@@ -28,11 +27,21 @@ public class Solution {
             tieBreaker = A.get(n-1);
         }
 
-        //A has to be a even length array for us to return tieBreaker
-        //If all the elements gets discarded in an even length array, 
-        //.. that means we return tieBreaker, which is either the majorityElement if present or -1 if majorityElement isnt present
-        boolean isAEven = !isAOdd;
-        if(B.isEmpty() && isAEven) {
+        /**
+         * Code according to the pseudocode
+         *      if(B.isEmpty()) {
+         *          return tieBreaker;
+         *      }
+         *
+         * Modification #1
+         * Need to also check here that the List A should not be of odd length
+         * Because if A was of odd length, then by returning the tieBreaker, we miss processing of the tieBreaker element
+         * that if the tieBreaker was the majority or not by executing the countOccurrences method on it
+         *
+         * However, if A is of even length, and B gets empty, then we are sure to have processed all the elements
+         * and therefore can return tieBreaker to parent function for further process.
+         */
+        if(B.isEmpty() && !isAOdd) {
             return tieBreaker;
         }
 
@@ -42,18 +51,28 @@ public class Solution {
             return -1;
         }
 
-        int count = countOccurences(A, c);
+        int count = countOccurrences(A, c);
 
-        //Since we are already counting the occurrences of the entire array
-        //.. there is no need to check the latter part of the below condition
-        if(count > n/2) { // || (count==n/2 && c==tieBreaker)) {
+        /**
+         * Code according to the pseudocode
+         *      if(count > n/2 || (count==n/2 && c==tieBreaker)) {
+         *             return c;
+         *      }
+         *
+         * Modification #2
+         * For the second part of the if condition, Need to also check here that the List A should not be of odd length
+         * This is because now with the addition of Modification #1, count contains the instance of tieBreaker, for
+         * cases when the length of the List A is odd.
+         * So when A's length is odd, the count has to be greater than n/2
+         */
+        if(count > n/2 || (!isAOdd && count==n/2 && c==tieBreaker)) {
             return c;
         }
 
         return -1;
     }
 
-    private static int countOccurences(List<Integer> A, int c) {
+    private static int countOccurrences(List<Integer> A, int c) {
         int res = 0;
         for(int i : A) {
             if(i == c) {
@@ -61,5 +80,16 @@ public class Solution {
             }
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = new int[]{1, 2, 3};
+        int ans1 = findMajority(nums1, 3);
+        System.out.println(ans1);
+
+        int[] nums2 = new int[]{1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2};
+        int ans2 = findMajority(nums2, 11);
+        System.out.println(ans2);
     }
 }
